381a0d3156dd8303498e98df25ea1a4d7ed62a02
Connor <c.mccurrie@gmail.com>
2017-02-15 21:08:03 -0600

    V 1.0
    Having difficulties posting due to URL issues.

---
+++ Quiz 3 Blog/blog.py
@@ -0,0 +1,50 @@
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, template, **params):
+         t = jinja_env.get_template(template)
+         return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    blogpost = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", blogpost="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM BlogPost "
+                           "ORDER BY created DESC")
+        
+        self.render("front-page.html", title=title, blogpost=blogpost, error=error, blogs=blogs)
+    
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        blogpost = self.request.get("blogpost")
+
+        if title and blogpost:
+            a = BlogPost(title = title, blogpost = blogpost)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(title, blogpost, error)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
