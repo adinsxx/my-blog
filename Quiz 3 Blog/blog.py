6b30affd0bb213cd57d9c2327ccbf36ca4cd808f
Connor <c.mccurrie@gmail.com>
2017-02-16 19:13:41 -0600

    V1.1

--- Quiz 3 Blog/blog.py
+++ Quiz 3 Blog/blog.py
@@ -1,4 +1,7 @@
 import os
+import re
+from string import letters
+
 import webapp2
 import jinja2
 
@@ -8,43 +11,152 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-class Handler(webapp2.RequestHandler):
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-    
+
     def render_str(self, template, **params):
-         t = jinja_env.get_template(template)
-         return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class BlogPost(db.Model):
-    title = db.StringProperty(required = True)
-    blogpost = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
 
-class MainPage(Handler):
-    def render_front(self, title="", blogpost="", error=""):
-        blogs = db.GqlQuery("SELECT * FROM BlogPost "
-                           "ORDER BY created DESC")
-        
-        self.render("front-page.html", title=title, blogpost=blogpost, error=error, blogs=blogs)
-    
+class MainPage(BlogHandler):
+  def get(self):
+      self.write('Hello, Udacity!')
+
+##### blog stuff
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class BlogFront(BlogHandler):
     def get(self):
-        self.render_front()
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front.html', posts = posts)
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
 
     def post(self):
-        title = self.request.get("title")
-        blogpost = self.request.get("blogpost")
+        subject = self.request.get('subject')
+        content = self.request.get('content')
 
-        if title and blogpost:
-            a = BlogPost(title = title, blogpost = blogpost)
-            a.put()
-
-            self.redirect("/")
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
         else:
-            error = "we need both a title and some artwork!"
-            self.render_front(title, blogpost, error)
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
+
+###### Unit 2 HW's
+class Rot13(BlogHandler):
+    def get(self):
+        self.render('rot13-form.html')
+
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+
+        self.render('rot13-form.html', text = rot13)
+
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Signup(BlogHandler):
+
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/unit2/welcome?username=' + username)
+
+class Welcome(BlogHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/unit2/signup')
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/unit2/rot13', Rot13),
+                               ('/unit2/signup', Signup),
+                               ('/unit2/welcome', Welcome),
+                               ('/blog/?', BlogFront),
+                               ('/blog/([0-9]+)', PostPage),
+                               ('/blog/newpost', NewPost),
+                               ],
+                              debug=True)
